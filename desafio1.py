61e3f40842d8d06976f0ba0dbee3dc3434bcd4cf
wagnerabelramos-cyber <wagner.abel.ramos@escola.pr.gov.br>
2026-03-25 08:01:53 -0300

    Update and rename desafio 1 to desafio1.py

--- desafio1.py
+++ desafio1.py
@@ -1,4 +1,10 @@
+#identifica os numeros escolhidos
+
 n1 = float(input("escolha um numero "))
 n2 = float(input("escolha outro numero "))
+
+
+#faz a soma dos numeros escolhidos
+
 soma = (n1 + n2)
 print("seu valor somado é", soma)
