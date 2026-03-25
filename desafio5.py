3cfd8e1bb117f963e352dec29707ef681d11e937
wagnerabelramos-cyber <wagner.abel.ramos@escola.pr.gov.br>
2026-03-25 08:16:45 -0300

    Update and rename desafio 5 to desafio5.py

--- desafio5.py
+++ desafio5.py
@@ -1,6 +1,10 @@
-#CALCULAR AREA DO TRIANGULO
+#pede ao usuario para informar a base do triangulo
 
 base = float(input("qual o tamanho da base de seu triangulo? ")) 
+
+#pede para informar a altura do triangulo
 altura = float(input("qual a altura de seu triangulo? "))
+
+#calcula a area usando a formula pra isso 
 formula = (base * altura) / 2
 print(formula)
