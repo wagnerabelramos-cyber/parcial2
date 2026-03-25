237da80a85dbacfbdfc7f15cc408830a2fda8aec
wagnerabelramos-cyber <wagner.abel.ramos@escola.pr.gov.br>
2026-03-25 08:07:08 -0300

    Update and rename desafio 3 to desafio3.py

--- desafio3.py
+++ desafio3.py
@@ -1,4 +1,5 @@
-#LISTA DE 5 COISAS AI   
-
+# deixa a lista montada com os objetos  
 objetos_de_apoio = ["cadeira de rodas","bengala","muletas","andador","protese"]
+
+# mostra os objetos de dentro da lista
 print(objetos_de_apoio)
