e86f6ad0bbcdc6cf7b81a225883e8d154324377b
wagnerabelramos-cyber <wagner.abel.ramos@escola.pr.gov.br>
2026-03-25 08:21:39 -0300

    Update and rename desafio 7 to desafio7.py

--- desafio7.py
+++ desafio7.py
@@ -1,7 +1,16 @@
+#pede para informar o capital
+
 C = float(input("quanto de capital você possui?    "))
+
+#pede para informar a taxa
 I = float(input("quanto qual a taxa?    "))
+
+#pede para informar o tempo
 T = float(input("em quanto tempo?    "))
+
+#usa a formula de juros simples
 
 J = (C *I* T) / 100 
 
+# mostra o resultado 
 print(J)
